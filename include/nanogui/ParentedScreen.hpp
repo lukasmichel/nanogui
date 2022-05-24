95e8771b3e7cec297caa61c61607cea93588f825
Lukas Michel <lukas@michel-nk.de>
2022-05-24 21:32:45 +0200

    update submodules

---
+++ include/nanogui/ParentedScreen.hpp
@@ -0,0 +1,18 @@
+//
+// Created by lukas on 20.05.22.
+//
+
+
+#pragma once
+
+#include <nanogui/widget.h>
+#include <nanogui/texture.h>
+#include <nanogui/screen.h>
+#include <nanogui/widget.h>
+class Texture;
+
+NAMESPACE_BEGIN(nanogui)
+class WindowScreen : public Screen{
+
+};
+NAMESPACE_END(nanogui)
